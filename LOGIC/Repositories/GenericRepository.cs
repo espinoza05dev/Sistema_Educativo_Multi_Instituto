9d37cd7b9871c970afd833885a88bc57f70096f7
Sbrentor <githubpersonal914@gmail.com>
2025-06-10 19:44:44 -0600

    RGeneric hecho

---
+++ LOGIC/Repositories/GenericRepository.cs
@@ -0,0 +1,67 @@
+﻿using DATA.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LOGIC.Repositories
+{
+    public class GenericRepository<T> where T : class
+    {
+        protected readonly SistemaEducativoContext _context;
+        protected readonly DbSet<T> _dbSet;
+        
+        public GenericRepository(SistemaEducativoContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity == null) throw new KeyNotFoundException($"Entity with ID {id} not found.");
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            return await _dbSet.AnyAsync(e => EF.Property<int>(e, "Id") == id);
+        }
+
+        public async Task<int> CountAsync()
+        {
+            return await _dbSet.CountAsync();
+
+        }
+}
